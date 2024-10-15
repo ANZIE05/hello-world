c5ff2932f4bd6f9747ce978e100e5192a24268db
Annie <161513339+ANZIE05@users.noreply.github.com>
2024-10-15 18:44:00 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HelloWold(){
+  return (
+     
+      <div className="bg-purple-950 h-screen">
+      <h1 className="pt-5 text-center font-serif text-6xl text-gray-300">Hello wolrd!</h1>
+      <p className="pt-48 text-center font-sans text-2xl text-gray-300">"I am Qurat Ul Ain. This is my first project on Next.js.."</p>
+    </div>
+  )
+}
