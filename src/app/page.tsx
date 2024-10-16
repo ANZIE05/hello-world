3e25f3b31679d58f13cae3a68fd12e0a81666bce
Annie <161513339+ANZIE05@users.noreply.github.com>
2024-10-17 02:27:47 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function HelloWold(){
      
       <div className="bg-purple-950 h-screen">
       <h1 className="pt-5 text-center font-serif text-6xl text-gray-300">Hello wolrd!</h1>
-      <p className="pt-48 text-center font-sans text-2xl text-gray-300">"I am Qurat Ul Ain. This is my first project on Next.js.."</p>
+      <p className="pt-48 text-center font-sans text-2xl text-gray-300">&quot;I am Qurat Ul Ain. This is my first project on Next.js..&quot;</p>
     </div>
   )
 }
